dbce046b3ff2609c7ca9ff9f935c1fb9e6962d7a
jdksjfisdf <wanghaoran505@126.com>
2025-06-10 14:42:06 +0800

    loopvectorization

--- src/SimplePIMC.jl
+++ src/SimplePIMC.jl
@@ -3,6 +3,7 @@ module SimplePIMC
 export simulate, simulate_correlation, simulate_correlation_general, IChain
 
 using LinearAlgebra, FileIO, Statistics
+using LoopVectorization
 
 include("types.jl")
 
@@ -33,9 +34,9 @@ function MC_update!(ichain::IChain; M, τ, masses, DOF, P, V)
     accept_prob = exp(-τ * (V1 - V0))
     accept = rand() < accept_prob
     if accept
-        @simd for n in 1:M-1
-            @inbounds ichain.coodinates[mod1(n + i, P), :] .= new_beads[n]
-            @inbounds ichain.energies[mod1(n + i, P)] = V1s[n]
+        @turbo for n in 1:M-1
+            ichain.coodinates[mod1(n + i, P), :] .= new_beads[n]
+            ichain.energies[mod1(n + i, P)] = V1s[n]
         end
     end
     return accept
